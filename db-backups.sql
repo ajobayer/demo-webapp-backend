37523382e398954067b2ee85d9d1769e2fd5766f
Abdullah Jobayer <ajobayer@gmail.com>
2017-12-05 14:09:16 +1100

    add data-base backup file

---
+++ db-backups.sql
@@ -0,0 +1,19 @@
+/*
+-- Query: SELECT * FROM test.users
+LIMIT 0, 1000
+
+-- Date: 2017-12-05 14:07
+*/
+INSERT INTO `users` (`id`,`name`,`age`,`email`) VALUES (1,'Jobayer',30,'ajobayer@gmail');
+INSERT INTO `users` (`id`,`name`,`age`,`email`) VALUES (3,'Mamun',35,'mamun@gmail.com');
+INSERT INTO `users` (`id`,`name`,`age`,`email`) VALUES (4,'Russel',18,'russel@yahoo.com');
+INSERT INTO `users` (`id`,`name`,`age`,`email`) VALUES (5,'Max',55,'max@itil.com');
+INSERT INTO `users` (`id`,`name`,`age`,`email`) VALUES (6,'Moly',34,'None');
+INSERT INTO `users` (`id`,`name`,`age`,`email`) VALUES (7,'Poly',30,'None');
+INSERT INTO `users` (`id`,`name`,`age`,`email`) VALUES (8,'Jelly',20,'None');
+INSERT INTO `users` (`id`,`name`,`age`,`email`) VALUES (9,'Walls',20,'None');
+INSERT INTO `users` (`id`,`name`,`age`,`email`) VALUES (10,'William',25,'william@gmail.com');
+INSERT INTO `users` (`id`,`name`,`age`,`email`) VALUES (11,'Jack',45,'jack@gmail.com');
+INSERT INTO `users` (`id`,`name`,`age`,`email`) VALUES (12,'Dillir',23,'dillir@gmail.com');
+INSERT INTO `users` (`id`,`name`,`age`,`email`) VALUES (13,'Sumi',33,'sumi@gmail.com');
+INSERT INTO `users` (`id`,`name`,`age`,`email`) VALUES (14,'Danial',60,'danial@hotmail.com');
