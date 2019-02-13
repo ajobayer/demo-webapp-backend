4b23652fd8f56ab3787cf638dc1dd80a710d0b93
Abdullah Jobayer <ajobayer@gmail.com>
2019-02-13 14:31:13 +1100

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,10 @@
-# mysql -h testprivate.ckrg1bdwvkzk.us-east-1.rds.amazonaws.com -P 3306 -u root1234 -p
-
+# client machine <AppServer>
 # yum install -y mysql-server mysql-client
 # create database test;
+
+# Databse connection from client machine  <AppServer>;
+# mysql -h testprivate.ckrg1bdwvkzk.us-east-1.rds.amazonaws.com -P 3306 -u root1234 -p
+
 # source createtable.sql;sss
 # source inserttable.sql;
 
